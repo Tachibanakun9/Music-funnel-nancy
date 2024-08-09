88d315a2bb0103c5cf67ba090a83f39070a1f843
Tachibanakun9 <167691698+Tachibanakun9@users.noreply.github.com>
2024-08-09 10:46:24 +0000

    xxx

--- src/auth.ts
+++ src/auth.ts
@@ -7,6 +7,7 @@ import Resend from "next-auth/providers/resend";
 import prisma from "./lib/prisma";
 
 
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   trustHost: true,
   theme: {
